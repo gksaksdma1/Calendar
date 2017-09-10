2688a09f2d6e21e1a0d58fd237f5ce1d8f10cbca
gksaksdma1 <gksaksdma1@naver.com>
2017-09-10 23:51:28 +0900

    Initial commit

---
+++ app/src/main/java/com/example/joon/calendar/MonthItem.java
@@ -0,0 +1,14 @@
+package com.example.joon.calendar;
+
+/**
+ * Created by JOON on 2017-07-19.
+ */
+
+public class MonthItem {
+
+    int day;
+
+    public MonthItem(int day) {
+        this.day = day;
+    }
+}
---
+++ app/src/main/java/com/example/joon/calendar/MonthItemView.java
@@ -0,0 +1,40 @@
+package com.example.joon.calendar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by JOON on 2017-07-19.
+ */
+
+public class MonthItemView extends RelativeLayout {
+
+    TextView textView;
+
+
+    public MonthItemView(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    public MonthItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.month_item, this, true);
+
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void setDay(int day) {
+        textView.setText(String.valueOf(day));
+    }
+}
